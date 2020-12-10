0c94cb90d92a599cfacd8ff87fbb82d91f34fa19
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-10 00:42:02 -0500

    Re-asignando valor a variable JS

--- JS/index.js
+++ JS/index.js
@@ -19,5 +19,8 @@ console.log('Hola Mundo')
 let miPrimeraVariable = 'Mi primera variable!'
 console.log(miPrimeraVariable);
 
+miPrimeraVariable = 'Esto ha cambiado'
+console.log(miPrimeraVariable);
+
 
 

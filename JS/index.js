2ed71a136541e07b8f83ec3db84121d2fb73501b
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-11 00:17:28 -0500

    Más tipos de datos

--- JS/index.js
+++ JS/index.js
@@ -19,8 +19,27 @@ console.log('Hola Mundo')
 let miPrimeraVariable = 'Mi primera variable!'
 console.log(miPrimeraVariable);
 
+// mutabilidad
 miPrimeraVariable = 'Esto ha cambiado'
 console.log(miPrimeraVariable);
+
+// boolean
+let miBoolean = true
+let miOtroBool = false
+
+// numeros
+let miNumero = 0
+let miNumero2 = 12
+let miNumero3 = -258
+
+console.log(miNumero, miNumero2, miBoolean, miPrimeraVariable)
+
+let undef
+console.log(undef);
+
+let nulo = null
+console.log('nulo',12, nulo)
+
 
 
 

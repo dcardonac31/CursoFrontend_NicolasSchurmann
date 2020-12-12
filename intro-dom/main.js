cfe8d018b7c335d0c30680ac2d64f67303cf011d
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-12 16:57:57 -0500

    Agregando los elementos a nuestro listado

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,10 +1,17 @@
+const todos = []
+
 window.onload = () =>{
- const form = document.getElementById('todo-form')
+ const form = document.getElementById('todo-form');
  form.onsubmit = (e) => {
    e.preventDefault();
    const todo = document.getElementById('todo');
    const todoText = todo.value;
    todo.value = '';
-   console.log(todoText);
+   todos.push(todoText);
+   const todoList = document.getElementById('todo-list')
+   todoList.innerHTML = '';
+   for(let i = 0; i < todos.length; i++){
+    todoList.innerHTML += '<li>' + todos[i] + '</li>'
+   }
  }
 }

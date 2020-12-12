99fa3bd95b0fada9044b2daad28727c619506830
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-12 17:26:03 -0500

    Simplificando con .map

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -9,9 +9,7 @@ window.onload = () =>{
    todo.value = '';
    todos.push(todoText);
    const todoList = document.getElementById('todo-list')
-   todoList.innerHTML = '';
-   for(let i = 0; i < todos.length; i++){
-    todoList.innerHTML += '<li>' + todos[i] + '</li>'
-   }
+   const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+   todoList.innerHTML = todosTemplate.join('');
  }
 }

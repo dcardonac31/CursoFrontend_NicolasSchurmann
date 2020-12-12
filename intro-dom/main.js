b94889987ceaed9a5393e062a0adfaefd206be5b
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-12 18:40:59 -0500

    Creando nuestra función de render

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,5 +1,19 @@
 const todos = []
 
+const render = () => {
+  const todoList = document.getElementById('todo-list')
+  const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+  todoList.innerHTML = todosTemplate.join('');
+  const elementos = document.querySelectorAll('#todo-list li')
+  elementos.forEach((elemento,i) => {
+   elemento.addEventListener('click',() =>{
+     elemento.parentNode.removeChild(elemento)
+     todos.splice(i, 1)
+     render()
+   })
+  }) 
+}
+
 window.onload = () =>{
  const form = document.getElementById('todo-form');
  form.onsubmit = (e) => {
@@ -8,16 +22,6 @@ window.onload = () =>{
    const todoText = todo.value;
    todo.value = '';
    todos.push(todoText);
-   const todoList = document.getElementById('todo-list')
-   const todosTemplate = todos.map(t => '<li>' + t + '</li>');
-   todoList.innerHTML = todosTemplate.join('');
-   const elementos = document.querySelectorAll('#todo-list li')
-   elementos.forEach((elemento,i) => {
-    elemento.addEventListener('click',() =>{
-      elemento.parentNode.removeChild(elemento)
-      todos.splice(i, 1)
-      console.log(todos, i)
-    })
-   }) 
+   render()
  }
 }

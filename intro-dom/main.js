a527fe9302018d122c6175b700edee91270ee254
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-12 16:48:14 -0500

    Obteniendo valores de un formulario

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,3 +1,10 @@
 window.onload = () =>{
- 
+ const form = document.getElementById('todo-form')
+ form.onsubmit = (e) => {
+   e.preventDefault();
+   const todo = document.getElementById('todo');
+   const todoText = todo.value;
+   todo.value = '';
+   console.log(todoText);
+ }
 }

297e8a6fcac0fd92dc68b64021356f608a3b7394
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-12 14:17:51 -0500

    Callbacks

--- JS/index.js
+++ JS/index.js
@@ -195,26 +195,37 @@ switch (y) {
 //   console.log('index ['+i+']: '+numeros[i]);
 // }
 
-function iterar(arg1) {
-  for (let i = 0; i < numeros.length; i++) {
-    console.log(arg1[i]);
-  }
+// function iterar(arg1) {
+//   for (let i = 0; i < numeros.length; i++) {
+//     console.log(arg1[i]);
+//   }
+// }
+
+// const numeros = [1, 2, 'Hola', 4, 5]
+// const nombres = ['Pedro', 'Juan', 'Felipe', 'Chanchito Feliz', 'Chanchito triste']
+
+// //iterar(numeros)
+// //iterar(nombres)
+
+// function suma(a, b) {
+//   return a + b;
+// }
+
+// const resultadoSuma1 = suma(1,2)
+// const resultadoSuma2 = suma(5,6)
+// const resultadoSuma3 = suma(resultadoSuma1,resultadoSuma2)
+
+// console.log('resultado', resultadoSuma3)
+
+function sumar(a,b,cb) {
+  const r = a + b
+  cb(r)
 }
 
-const numeros = [1, 2, 'Hola', 4, 5]
-const nombres = ['Pedro', 'Juan', 'Felipe', 'Chanchito Feliz', 'Chanchito triste']
-
-//iterar(numeros)
-//iterar(nombres)
-
-function suma(a, b) {
-  return a + b;
+function callback(result){
+  console.log('resultado', result)
 }
 
-const resultadoSuma1 = suma(1,2)
-const resultadoSuma2 = suma(5,6)
-const resultadoSuma3 = suma(resultadoSuma1,resultadoSuma2)
-
-console.log('resultado', resultadoSuma3)
+sumar(2,3, callback)
 
 

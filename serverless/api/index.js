2628efe71dcc2c439105be91d83947c0349f6b99
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-13 23:30:31 -0500

    Instalando dependencias y conectandose a mongo atlas

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
-  res.send('Hola mundo')
-}
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.get('*',(req, res) =>{
+  res.send('chanchito feliz')
+})
+module.exports = app
+

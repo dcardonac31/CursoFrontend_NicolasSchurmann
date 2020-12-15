b058405d69455d47da7410c03353f45fcd9e8816
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-15 13:01:08 -0500

    Agregando interacción con mongo DB

--- serverless/api/routes/meals.js
+++ serverless/api/routes/meals.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const Meals = require('../models/Meals')
 
-const router = app.router()
+const router = app.Router()
+
 router.get('/',(req, res) =>{
-  res.send('hola meals')
+  Meals.find()
+    .exec()
+    .then(x => res.status(200).send(x))
+})
+
+router.get('/:id', (req, res) => {
+  Meals.findById(req.params.id)
+    .exec()
+    .then(x => res.status(200).send(x))
 })
 
 router.post('/', (req, res) =>{
-  res.send('post meals')
+  Meals.create(req.body).then(x => res.status(201).send(x))
+})
+
+router.put('/:id', (req, res) => {
+  Meals.findOneAndUpdate(req.params.id, req.body)
+  .then(() => res.sendStatus(204))
+})
+
+router.delete('/:id', (req, res) => {
+  Meals.findOneAndDelete(req.params.id).exec().then(() => res.sendStatus(204))
 })
 
 module.exports = router

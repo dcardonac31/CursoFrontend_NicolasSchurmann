64668b290432fc4397cc7664ae13145847f728cf
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-15 12:12:32 -0500

    Configurando para ejecutar la aplicación en local

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get('*',(req, res) =>{
-  res.send('chanchito feliz')
-})
+  ressend('chanchito feliz')
+}).
 module.exports = app
 

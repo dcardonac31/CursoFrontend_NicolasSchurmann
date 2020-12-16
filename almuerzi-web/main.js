ae38968dbc67fad35970d68dcd5da0371a4e25b3
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-15 23:33:51 -0500

    Seleccionando las meals - parte 1: template strings

--- almuerzi-web/main.js
+++ almuerzi-web/main.js
@@ -1,10 +1,14 @@
+const renderItem = (item) => {
+  return `<li data-id="${item._id}">${item.name}</li>`
+}
+
 window.onload = () => {
   fetch('https://serverless.dcardonac31.vercel.app/api/meals')
     .then(response => response.json())
     .then(data => {
       const mealsList = document.getElementById('meals-list')
       const submit = document.getElementById('submit')
-      const template = data.map(x => '<li>' + x.name + '</li>').join('')
+      const template = data.map(renderItem).join('')
       mealsList.innerHTML = template
       submit.removeAttribute('disabled')
     })

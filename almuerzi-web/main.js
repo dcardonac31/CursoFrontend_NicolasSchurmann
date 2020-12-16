24c73000f129906499a00d4e99d5c28103ac3fdf
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-15 20:21:42 -0500

    Mostrando listado de meals

--- almuerzi-web/main.js
+++ almuerzi-web/main.js
@@ -1,5 +1,9 @@
 window.onload = () => {
   fetch('https://serverless.dcardonac31.vercel.app/api/meals')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      const mealsList = document.getElementById('meals-list')
+      const template = data.map(x => '<li>' + x.name + '</li>').join('')
+      mealsList.innerHTML = template
+    })
 }

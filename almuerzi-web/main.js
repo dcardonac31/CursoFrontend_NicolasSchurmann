c20174027ada544f050737001c4f68bd66b678fe
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-15 20:28:33 -0500

    Desabilitando botón

--- almuerzi-web/main.js
+++ almuerzi-web/main.js
@@ -3,7 +3,9 @@ window.onload = () => {
     .then(response => response.json())
     .then(data => {
       const mealsList = document.getElementById('meals-list')
+      const submit = document.getElementById('submit')
       const template = data.map(x => '<li>' + x.name + '</li>').join('')
       mealsList.innerHTML = template
+      submit.removeAttribute('disabled')
     })
 }

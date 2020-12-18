cd0c07cc58bb9beb0201b7abf8e9ba17100263cd
David Sneider Cardona Cardenas <david_ec10@hotmail.com>
2020-12-17 19:50:51 -0500

    Preparando y probando las rutas

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const meals = require('./routes/meals')
 const orders = require('./routes/orders')
+const auth = require('./routes/auth')
 
 const app = express()
 app.use(bodyParser.json())
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 
 app.use('/api/meals', meals)
 app.use('/api/orders', orders)
+app.use('/api/auth', auth)
 
 module.exports = app
 
